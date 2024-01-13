1be890e433ca0ecd3fd07b650f544f53290d8684
Noah Morales <nmorale5@mit.edu>
2024-01-13 12:40:18 -0500

    scroll down trick to hide address bar

--- index.js
+++ index.js
@@ -15,17 +15,21 @@ navigator.mediaDevices.getUserMedia({
     console.error('Error accessing rear-facing camera:', error);
   });
 
-document.getElementById("fullscreen-button").addEventListener("click", (ev) => {
-    // Check if the browser supports the Fullscreen API
-    if (video.requestFullscreen) {
-        // Request fullscreen
-        console.log("going fullscreen")
-        video.requestFullscreen();
-    } else if (video.mozRequestFullScreen) { // Firefox
-        video.mozRequestFullScreen();
-    } else if (video.webkitRequestFullscreen) { // Chrome, Safari and Opera
-        video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) { // Internet Explorer
-        video.msRequestFullscreen();
-    }
-});
+// document.getElementById("fullscreen-button").addEventListener("click", (ev) => {
+//     // Check if the browser supports the Fullscreen API
+//     if (video.requestFullscreen) {
+//         // Request fullscreen
+//         console.log("going fullscreen")
+//         video.requestFullscreen();
+//     } else if (video.mozRequestFullScreen) { // Firefox
+//         video.mozRequestFullScreen();
+//     } else if (video.webkitRequestFullscreen) { // Chrome, Safari and Opera
+//         video.webkitRequestFullscreen();
+//     } else if (video.msRequestFullscreen) { // Internet Explorer
+//         video.msRequestFullscreen();
+//     }
+// });
+
+addEventListener("load", function() {
+    setInterval(() => window.scrollTo(0, 1), 10);
+}, false);
