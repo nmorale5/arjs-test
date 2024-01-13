66a2b8da9fd2a6e71eda9f7db905394639b405fb
Noah Morales <nmorale5@mit.edu>
2024-01-12 22:23:46 -0500

    added correct js

--- index.js
+++ index.js
@@ -1,65 +1,38 @@
-import * as THREE from 'three';
-import * as THREEx from './node_modules/@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js';
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
 
-function main() {
-    const canvas = document.getElementById('canvas1');
-
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
-    const renderer = new THREE.WebGLRenderer({canvas: canvas});
-
-    const arjs = new THREEx.LocationBased(scene, camera);
-    const cam = new THREEx.WebcamRenderer(renderer);
-
-    const geom = new THREE.BoxGeometry(20, 20, 20);
-    const mtl = new THREE.MeshBasicMaterial({color: 0xff0000});
-    const box = new THREE.Mesh(geom, mtl);
-    arjs.add(box, -0.72, 51.051); 
-
-
-    arjs.fakeGps(-0.72, 51.05);
-
-    requestAnimationFrame(render);
-
-    const rotationStep = THREE.Math.degToRad(2);
-
-    let mousedown = false, lastX =0;
-
-    window.addEventListener("mousedown", e=> {
-        mousedown = true;
-    });
-
-    window.addEventListener("mouseup", e=> {
-        mousedown = false;
-    });
-
-    window.addEventListener("mousemove", e=> {
-        if(!mousedown) return;
-        if(e.clientX < lastX) {
-            camera.rotation.y -= rotationStep;
-            if(camera.rotation.y < 0) {
-                camera.rotation.y += 2 * Math.PI;
-            }
-        } else if (e.clientX > lastX) {
-            camera.rotation.y += rotationStep;
-            if(camera.rotation.y > 2 * Math.PI) {
-                camera.rotation.y -= 2 * Math.PI;
-            }
-        }
-        lastX = e.clientX;
-    });
-
-    function render() {
-        if(canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
-            renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-            const aspect = canvas.clientWidth/canvas.clientHeight;
-            camera.aspect = aspect;
-            camera.updateProjectionMatrix();
-        }
-        cam.update();
-        renderer.render(scene, camera);
-        requestAnimationFrame(render);
-    }
+function staticLoadPlaces() {
+   return [
+       {
+           name: 'Magnemite',
+           location: {
+               lat: 40.713013,
+               lng: -73.885364,
+           }
+       },
+   ];
 }
 
-main();
+function renderPlaces(places) {
+   let scene = document.querySelector('a-scene');
+
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
+
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+       model.setAttribute('rotation', '0 180 0');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '0.5 0.5 0.5');
+
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
+
+       scene.appendChild(model);
+   });
+}
